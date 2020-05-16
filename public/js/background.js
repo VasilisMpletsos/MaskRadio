cfc721e0b2368e4939033143069aece8b07b63bb
VasilisMpletsos <msa17020@gmail.com>
2020-05-16 13:50:09 +0300

    Added 25 more backgrounds to have in total 40 and seem like total random.

--- public/js/background.js
+++ public/js/background.js
@@ -14,7 +14,32 @@ const background = {
     back12: 'ysA6qL8j-OI',
     back13: 'vC8wj_Kphak',
     back14: '5J1bHRX1fNw',
-    back15: 'G9vRDGA46FY'
+    back15: 'G9vRDGA46FY',
+    back16: 'J_F_003jcEQ',
+    back17: 'bJlcz7DONyk',
+    back18: 't_huLiSJMX8',
+    back19: 'usmhPmT5sj0',
+    back20: '023T4jyCRqA',
+    back21: 'NaGoAsPP0wc',
+    back22: 'd5SZqLkpIrY',
+    back23: 't05kfHeygbE',
+    back24: 'wB9WormrY1o',
+    back25: 'BLZqzDZ02lI',
+    back26: 'CT8NvobyYuk',
+    back27: 'z4X3yABcf5g',
+    back28: 'OA75EP70hPQ',
+    back29: 'fQIgeD6UfBw',
+    back30: '00yDgACVeMA',
+    back31: 'bkAFib_51cE',
+    back32: 'tNUbMtM71Lk',
+    back33: 'wzWgRXoyhT0',
+    back34: 'xrzHZfJ7lxQ',
+    back35: '2tX5Wgh5XIA',
+    back36: 'OJYwC5JC7Bs',
+    back37: 'k9uEJ3eke50',
+    back38: 'uYewNGsTXPA',
+    back39: 'nk6vQ7Ma-Kw',
+    back40: 'vXjhyR_v0J4'
   }
 
   const length = Object.keys(background).length;
