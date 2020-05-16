02cd63bf446869624bb099f61a3ce271df0c97b9
VasilisMpletsos <msa17020@gmail.com>
2020-05-16 19:19:40 +0300

    Fixed Rotate of social buttons!

--- public/js/rotate.js
+++ public/js/rotate.js
@@ -2,9 +2,15 @@ var facebook = document.querySelector('.fa-facebook');
 var instagram = document.querySelector('.fa-instagram');
 
 facebook.addEventListener('mouseover',()=>{
-    facebook.style.transform = "rotate(360deg)";
+  facebook.style.transform = "rotate(360deg)";
+  setTimeout(function(){
+    facebook.style = "";
+  },2000)
 })
 
 instagram.addEventListener('mouseover',()=>{
-    instagram.style.transform = "rotate(360deg)";
+  instagram.style.transform = "rotate(360deg)";
+  setTimeout(function(){
+    instagram.style = "";
+  },2000)
 })
