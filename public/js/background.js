762d5d2f32f4a80185d9e2a6758f7d3afb6343d1
VasilisMpletsos <msa17020@gmail.com>
2020-05-16 12:00:51 +0300

    Added more options for background and removed snow its summer(It was fun thow)!

--- public/js/background.js
+++ public/js/background.js
@@ -9,7 +9,12 @@ const background = {
     back7: 'wKc-i5zwfok',
     back8: 'mPnxwQBtUZE',
     back9: 'mPnxwQBtUZE',
-    back10: '67sVPjK6Q7I'
+    back10: '67sVPjK6Q7I',
+    back11: 'npPNgsWDCrc',
+    back12: 'ysA6qL8j-OI',
+    back13: 'vC8wj_Kphak',
+    back14: '5J1bHRX1fNw',
+    back15: 'G9vRDGA46FY'
   }
 
   const length = Object.keys(background).length;
