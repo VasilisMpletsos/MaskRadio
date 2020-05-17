f6a030a9219466e6ce424227cba96ba4fcd252b9
VasilisMpletsos <msa17020@gmail.com>
2020-05-18 01:57:59 +0300

    There were a considerably amount of bugs when i implemented to work differently on small screens. Fixed Them! At last..

--- public/js/song.js
+++ public/js/song.js
@@ -4,17 +4,31 @@ $('#songForm').submit(function (e) {
   postToSite();
 });
 
+$('#songFormXs').submit(function (e) {
+  e.preventDefault();
+  addNotify();
+  postToSite();
+});
+
 function addNotify(){
-  const song = document.getElementById('songField');
-  const forLove = document.getElementById('forField');
+  var w = window.innerWidth;
+  if (w>728){
+    var song = document.getElementById('songField');
+    var forLove = document.getElementById('forField');
+    console.log(`Bigger than XS = ${w}`);
+  }else{
+    var song = document.getElementById('songFieldXs');
+    var forLove = document.getElementById('forFieldXs');
+    console.log(`Smaller than XS = ${w}`);
+  }
   const add = document.getElementById('notify');
   const success = document.createElement('div');
   if(forLove.value == ""){
     forLove.value = '-';
   }
   success.innerHTML = `<div class="alert alert-success">
-  <b><span class="glyphicon glyphicon-ok"></span></b><strong> Success!</strong> The song <b>${song.value}</b> for <b>${forLove.value} </b> has been sent.
-  </div>`;
+  <center><b><span class="glyphicon glyphicon-ok"></span></b><strong> Success!</strong> The song <b>${song.value}</b> for <b>${forLove.value} </b> has been sent.
+  </center></div>`;
   add.appendChild(success);
   setTimeout(function(){
         success.innerHTML = '';
@@ -40,13 +54,20 @@ function getCookie(cname) {
 }
 
 function postToSite() {
-  const song = $('#songField').val();
-  const dedicate = $('#forField').val()
-  console.log(song,dedicate);
+  var w2 = window.innerWidth;
+  if (w2>728){
+    var song2 = document.getElementById('songField').value;
+    var dedicate = document.getElementById('forField').value;
+  }else{
+    var song2 = document.getElementById('songFieldXs').value;
+    var dedicate = document.getElementById('forFieldXs').value;
+  }
+  console.log('Dedicating Song');
+  console.log(song2,dedicate);
   $.ajax({
     method: "POST",
     url: '/maskRadio',
-    data: {song:song, dedicate: dedicate, listener: getCookie("username")},
+    data: {song:song2, dedicate: dedicate, listener: getCookie("username")},
     success: function(){
       console.log('Success');
     }
