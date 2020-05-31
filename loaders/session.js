028383cc1618b41d73543e8ea92f5b839e9c4c06
Zacharopoulos Filippos <ziposc7@outlook.com>
2020-06-01 00:35:50 +0300

    Refactor and modularise

---
+++ loaders/session.js
@@ -0,0 +1,22 @@
+/*
+ * The sessian initialisation function.
+ * The MongoStore is responsible for saving the sessionId in the database.
+ */
+
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session);
+
+const { v4: uuid } = require('uuid'); // To generate unique random strings
+
+module.exports = (app, mongooseConnection) => {
+  app.use(session({
+    genid: (req) => {
+      console.log('Setting up a session with a client..')
+      return uuid() // The unique string identifier for the session.
+    },
+    store: new MongoStore({ mongooseConnection: mongooseConnection}),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  }));
+};
