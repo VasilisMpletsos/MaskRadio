17903789baeb76bf8b0b2bfc839f0196c9c54c46
Vasilis Mpletsos <58275997+VasilisMpletsos@users.noreply.github.com>
2020-06-06 00:58:44 +0300

    Now each song that is opened is crossovered! And i only update new songs!

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,9 @@
 var songList = document.getElementById('songsList');
+songList.innerHTML='';
+let rowHead = document.createElement('div');
+rowHead.setAttribute('class', 'row justify-content-center');
+rowHead.innerHTML = `<h2>Requested Songs</h2>`;
+songList.appendChild(rowHead);
 
 async function getSongs() {
   return await $.ajax({
@@ -10,40 +15,44 @@ async function getSongs() {
   });
 }
 
+var previousSongs = [];
 
 function displaySongs(songlist){
   getSongs().then((data)=>{
-    songlist.innerHTML='';
-    let rowHead = document.createElement('div');
-    rowHead.setAttribute('class', 'row justify-content-center');
-    rowHead.innerHTML = `<h2>Requested Songs</h2>`;
-    songlist.appendChild(rowHead);
+    if(previousSongs.length!==data.data.length){
+      start = previousSongs.length;
+      end = data.data.length;
+      previousSongs = data.data;
+      for(var i=start;i<end;i++){
+        let row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        row.setAttribute('id', 'parent');
 
-    data.data.forEach((song) => {
-      let row = document.createElement('div');
-      row.setAttribute('class', 'row');
-      row.setAttribute('id', 'parent');
+        let songThumbnail = document.createElement('img');
+        songThumbnail.setAttribute('class', 'col-xs-6 thumbnail');
+        songThumbnail.setAttribute('src', data.data[i].thumbnail);
+        row.appendChild(songThumbnail);
 
-      let songThumbnail = document.createElement('img');
-      songThumbnail.setAttribute('class', 'col-xs-6 thumbnail');
-      songThumbnail.setAttribute('src', song['thumbnail']);
-      row.appendChild(songThumbnail);
+        let link = document.createElement('a');
+        link.setAttribute('class', 'col-xs-8 vertical');
+        link.setAttribute('href', `https://www.youtube.com/watch?v=${data.data[i].id}`);
+        link.setAttribute('target', '_blank');
+        row.appendChild(link);
 
-      let link = document.createElement('a');
-      link.setAttribute('class', 'col-xs-8 vertical');
-      link.setAttribute('href', `https://www.youtube.com/watch?v=${song['id']}`);
-      link.setAttribute('target', '_blank');
-      row.appendChild(link);
+        let songTitle = document.createElement('div');
+        songTitle.setAttribute('class', 'songTitle');
+        songTitle.innerText = data.data[i].title.substring(0,30) + ' | ' + data.data[i].dedicate;
+        link.appendChild(songTitle);
 
-      let songTitle = document.createElement('div');
-      songTitle.setAttribute('class', 'songTitle');
-      songTitle.innerText = song['title'].substring(0,30) + ' | ' + song['dedicate'];
-      link.appendChild(songTitle);
+        link.addEventListener('click', () => {
+          songTitle.style.textDecoration = "line-through";
+        })
 
-      songlist.appendChild(row);
-    });
-
-  })
+        songlist.appendChild(row);
+      };
+    }
+  }
+  )
 }
 
 displaySongs(songList);
